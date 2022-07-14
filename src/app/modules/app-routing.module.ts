7bb736d9877307ebf5ce304e68c028c7512ffe39
ShaniGeva-Next <shani.g@next-insurance.com>
2022-07-14 19:43:13 +0300

    setting up project structure

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from '../welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path:'', redirectTo: 'welcome', pathMatch: 'full'
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
+    path: 'book-details', 
+    redirectTo: 'book-details/0'
+  },
+  { 
+    path:'**', redirectTo: 'welcome', pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
