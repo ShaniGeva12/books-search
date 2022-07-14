da72a3c9644b3b5a4e3d63ab4284c28c7419a734
ShaniGeva-Next <shani.g@next-insurance.com>
2022-07-14 18:25:00 +0300

    setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './modules/app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
