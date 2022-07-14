9f5e9976c035113031cf6a0ce4c89618826f4f79
ShaniGeva-Next <shani.g@next-insurance.com>
2022-07-14 19:43:31 +0300

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './modules/material.module';
+import { BooksComponent } from './books/books.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BooksComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
