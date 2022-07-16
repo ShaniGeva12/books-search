c691cb7142dd873e35b6d85bfec59ebb25c4b0d4
ShaniGeva-Next <shani.g@next-insurance.com>
2022-07-16 19:28:03 +0300

    set up welcome comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './modules/material.module';
 import { BooksComponent } from './books/books.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MaterialModule,
   ],
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  
+  welcomeForm!: FormGroup;
 
-  constructor() { }
+  subs: SubSink = new SubSink();
+
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.setForm();
+  }
+
+  setForm(){
+    this.welcomeForm = this.fb.group({
+      name: [ null,
+        Validators.required
+      ],
+    });
+  }
+
+  onSubmit(){
+    // this.booksService.addBook(<AddBookRequest>this.bookForm.value);
+    this.navigateToBooks();
+  }
+
+  navigateToBooks(){
+    // this.router.navigate(['/show-bands']);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
 }
